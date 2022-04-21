7aef6c6344267602152a8fc6dcc6933ca6eaeea2
Priyadharshini40 <104074666+Priyadharshini40@users.noreply.github.com>
2022-04-21 15:26:13 +0530

    Add files via upload

--- Voting.java
+++ Voting.java
@@ -1,1 +1,18 @@
-
+import java.util.Scanner;
+class Voting
+{
+public static void main(String[] args)
+{
+Scanner s=new Scanner(System.in);
+System.out.println("enter ur Age:");
+int a=s.nextInt();
+if (a>=18)
+{
+System.out.println("you are eligible to vote");
+}
+else
+{
+System.out.println("you are not eligible to vote");
+}
+}
+}
