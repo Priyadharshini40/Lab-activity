9b75003fd0958f6ec6d115eaddd5ffba786a4c0d
Priyadharshini40 <104074666+Priyadharshini40@users.noreply.github.com>
2022-05-11 09:15:03 +0530

    Add files via upload

---
+++ Singlearray.java
@@ -0,0 +1,28 @@
+/*class Singlearray
+{
+public static void main(String args[])
+{
+int a []={10,30,50};
+int sum=0,average=0;
+
+for(int i=0;i<a.length;i++)
+{
+sum=sum+a [i];
+average=a[i]/a.length;
+}
+System.out.println("sum of the array:" +sum );
+System.out.println("average of the array:" +average );
+
+}
+}*/
+class Singlearray
+{
+public static void main(String args[])
+{
+int a []={10,20,30};
+for(int b:a)
+{
+System.out.println(b);
+}
+}
+}
