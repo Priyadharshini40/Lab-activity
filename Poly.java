70cf371846a7ea527e3383c7a800378f99e54655
Priyadharshini40 <104074666+Priyadharshini40@users.noreply.github.com>
2022-05-12 14:14:23 +0530

    Add files via upload

---
+++ Poly.java
@@ -0,0 +1,21 @@
+class Runtimepoly
+{
+void display()
+{
+System.out.println("normal method");
+}
+}
+class Poly extends Runtimepoly
+{
+void display()
+{
+super.display();
+System.out.println("runtime polymorphism");
+
+}
+public static void main(String args[])
+{
+Poly p=new Poly();
+p.display();
+}
+}
